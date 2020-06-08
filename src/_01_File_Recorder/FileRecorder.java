f5831fac9591eac9abbe6ae855a736302216bc28
Sofia Velarde <sofiavelarde2004@gmail.com>
2020-06-08 11:27:09 -0700

    FileRecorder(IP)
    still working on it

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,15 @@
 package _01_File_Recorder;
 
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("Write anything for it to be saved into a file!");
+		
+		
+		
+		
+	}
+	
 }
