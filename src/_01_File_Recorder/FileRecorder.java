ef83e4fef196628ce20f47b765621deaf711f1b3
Sofia Velarde <sofiavelarde2004@gmail.com>
2020-06-09 11:30:31 -0700

    FileRecorder, FileEncryptor (IP)
    still working on file encryptor

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,15 +1,27 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.swing.JOptionPane;
 
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
 	public static void main(String[] args) {
-		JOptionPane.showInputDialog("Write anything for it to be saved into a file!");
-		
-		
-		
-		
+		String input = JOptionPane.showInputDialog("Write anything ");
+
+		try {
+
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/test3.txt", true);
+
+			fw.write("\nThis is me writing a message " + input);
+
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 }
